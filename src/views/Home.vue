5571571c37d0ffd34bb4435f457e5863aa2ed5b2
bjornum <bjoern-einar2@hotmail.no>
2022-07-07 08:33:39 +0200

    Resources - Removed The text home (was confusing) - Removed Podcast and Courses cards, not in use. * changed name of others -> external links and sound -> others

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -136,7 +136,6 @@
       </v-col>
 
       <!-- RSS -->
-      <!-- RSS -->
       <v-col cols="12" xl="2" lg="2" md="2" sm="12" xs="12" class="pt-0"></v-col>
       <v-col cols="12" xl="8" lg="8" md="8" sm="12" xs="12" class="pt-0">
         <RssForHome></RssForHome>
--- src/views/Resources.vue
+++ src/views/Resources.vue
@@ -15,10 +15,10 @@
             <v-card @click="displayResource(item.template)">
               <p v-if="item.title == 'Video' || item.title == 'Video'" class="text-center title pt-10 pb-10">{{ $t('resources.video') }}</p>
               <p v-else-if="item.title == 'Files' || item.title == 'Filer'" class="text-center title pt-10 pb-10">{{ $t('resources.files') }}</p>
-              <p v-else-if="item.title == 'Courses' || item.title == 'Kurs'" class="text-center title pt-10 pb-10">{{ $t('resources.courses') }}</p>
-              <p v-else-if="item.title == 'Podcast' || item.title == 'Podcast'" class="text-center title pt-10 pb-10">{{ $t('resources.podcast') }}</p>
-              <p v-else-if="item.title == 'Sound' || item.title == 'Lyd'" class="text-center title pt-10 pb-10">{{ $t('resources.sound') }}</p>
-              <p v-else-if="item.title == 'Other' || item.title == 'Annet'" class="text-center title pt-10 pb-10">{{ $t('resources.other') }}</p>
+              <!-- <p v-else-if="item.title == 'Courses' || item.title == 'Kurs'" class="text-center title pt-10 pb-10">{{ $t('resources.courses') }}</p>
+              <p v-else-if="item.title == 'Podcast' || item.title == 'Podcast'" class="text-center title pt-10 pb-10">{{ $t('resources.podcast') }}</p> -->
+              <p v-else-if="item.title == 'Other' || item.title == 'Lyd'" class="text-center title pt-10 pb-10">Other</p>
+              <p v-else-if="item.title == 'External Links' || item.title == 'Annet'" class="text-center title pt-10 pb-10">External Links</p>
               <p v-else>...</p>
               <v-divider class="newsCardDividerPositioning" width="98%" style="padding: 2px;" :style="`background-color:${colorArr[index]}`"></v-divider>
               <v-col cols="12" class="ma-0 pa-0" style="padding-top: 0.1px !important;"></v-col>
@@ -37,7 +37,7 @@
             <!-- Default -->
             <template v-if="resourceTemplate == 0">
               <v-col cols="12">
-                <p class="text-center title">HOME</p>
+                <!-- <p class="text-center title">HOME</p> -->
               </v-col>
             </template>
 
@@ -57,10 +57,10 @@
               <v-col cols="12" xl="5" lg="5" md="5" sm="12" xs="12" class="pb-0">
                 <p class=" title" v-if="resourceTemplate == 1">Video</p>
                 <p class=" title" v-else-if="resourceTemplate == 2">Files</p>
-                <p class=" title" v-else-if="resourceTemplate == 3">Courses</p>
-                <p class=" title" v-else-if="resourceTemplate == 4">Podcast</p>
-                <p class=" title" v-else-if="resourceTemplate == 5">Sound</p>
-                <p class=" title" v-else-if="resourceTemplate == 6">Other</p>
+                <!-- <p class=" title" v-else-if="resourceTemplate == 3">Courses</p>
+                <p class=" title" v-else-if="resourceTemplate == 4">Podcast</p> -->
+                <p class=" title" v-else-if="resourceTemplate == 5">Other</p>
+                <p class=" title" v-else-if="resourceTemplate == 6">External Link</p>
               </v-col>
                <v-col cols="12" xl="3" lg="3" md="3" sm="12" xs="12" class="d-flex align-end flex-column">
                 <v-btn class="mr-5" @click="closeTheResourceBox()">X</v-btn>
@@ -205,10 +205,10 @@ export default {
       resourceFolders:[
         { title: this.$t('resources.video'), colour: "", image: "", description: "", template: 1 },
         { title: this.$t('resources.files'), colour: "", image: "", description: "", template: 2 },
-        { title: this.$t('resources.courses'), colour: "", image: "", description: "", template: 3 },
-        { title: this.$i18n.t('resources.podcast'), colour: "", image: "", description: "", template: 4 },
-        { title: this.$i18n.t('resources.sound'), colour: "", image: "", description: "", template: 5 },
-        { title: this.$i18n.t('resources.other'), colour: "", image: "", description: "", template: 6 }
+        // { title: this.$t('resources.courses'), colour: "", image: "", description: "", template: 3 },
+        // { title: this.$i18n.t('resources.podcast'), colour: "", image: "", description: "", template: 4 },
+        { title: "Other", colour: "", image: "", description: "", template: 5 },
+        { title: "External Links", colour: "", image: "", description: "", template: 6 }
       ],
       /* Easy way to display the content
       
