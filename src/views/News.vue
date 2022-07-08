87fe57b5ff0e3d36f1cad62ac3216936d2e47ebb
bjornum <bjoern-einar2@hotmail.no>
2022-07-08 11:57:46 +0200

    .

--- src/views/News.vue
+++ src/views/News.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="homePageWidth">
-    <NewsDialog ref="openingNewsDialog"></NewsDialog>
     <v-row>
       <v-col cols="12" class="mt-15"></v-col>
       <v-col cols="12" xl="2" lg="2" md="2" sm="12" xs="12" class="pt-0"></v-col>
