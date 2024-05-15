b3101a89dbebfd026c766d9b45f3437ba2390a8f
bjornum <37101863+bjornum@users.noreply.github.com>
2024-05-15 08:55:37 +0200

    Fixed footer

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,39 @@
 <template>
-  <v-app>
-
+  <v-app style="display: flex; flex-direction: column; min-height: 100vh">
     <VisitorTracker></VisitorTracker>
-    <section>
-
-      <!-- <Sidebar v-if="$vuetify.breakpoint.mdAndDown"></Sidebar> -->
-      <!-- <Navbar v-else></Navbar> -->
-    </section>
-    <section>
-      <v-main class="containeraaaa">
-        <router-view/>
-      </v-main>
-    </section>
-    <!-- <NavbarPhone></NavbarPhone> -->
-    <section>
-      <Footer></Footer>
-    </section>
+    <v-main style="flex: 1">
+      <router-view />
+    </v-main>
+    <Footer></Footer>
   </v-app>
 </template>
 
 <script>
-import Navbar from "@/components/Global/Navbar.vue"
-import NavbarPhone from "@/components/Global/NavbarPhone.vue"
-import LanguageTest from "@/components/TestAndDevelopment/LanguageTest.vue"
-import SelectLocale from "@/components/TestAndDevelopment/SelectLocale.vue"
-import Sidebar from "@/components/Global/Sidebar.vue"
-import Footer from "@/components/Global/Footer.vue"
-import VisitorTracker from "@/components/VisitTracker/VisitorTracker.vue"
+import Navbar from "@/components/Global/Navbar.vue";
+import NavbarPhone from "@/components/Global/NavbarPhone.vue";
+import LanguageTest from "@/components/TestAndDevelopment/LanguageTest.vue";
+import SelectLocale from "@/components/TestAndDevelopment/SelectLocale.vue";
+import Sidebar from "@/components/Global/Sidebar.vue";
+import Footer from "@/components/Global/Footer.vue";
+import VisitorTracker from "@/components/VisitTracker/VisitorTracker.vue";
 
 export default {
-  name: 'App',
-  components:{
+  name: "App",
+  components: {
     Navbar,
     Sidebar,
     NavbarPhone,
     LanguageTest,
     SelectLocale,
     Footer,
-    VisitorTracker
+    VisitorTracker,
   },
-  data(){
-    return {
-      
-    }
-  }
+  data() {
+    return {};
+  },
 };
 </script>
 
 <style>
-  @import url('https://fonts.googleapis.com/css2?family=Barlow:wght@700&family=Lato:wght@400;700&display=swap');
-
-
+@import url("https://fonts.googleapis.com/css2?family=Barlow:wght@700&family=Lato:wght@400;700&display=swap");
 </style>
--- src/components/Global/Footer.vue
+++ src/components/Global/Footer.vue
@@ -5,58 +5,25 @@
         <v-divider class="dividerStyling"></v-divider>
       </v-col>
       <!-- Erasmus Logo -->
-      <v-col cols="12" xl="2" lg="2" md="2" sm="12" xs="12" v-if="$vuetify.breakpoint.mdAndDown">
+      <v-col
+        cols="12"
+        xl="2"
+        lg="2"
+        md="2"
+        sm="12"
+        xs="12"
+        v-if="$vuetify.breakpoint.mdAndDown"
+      >
         <v-img src="img/erasmusLogoFooter.jpg" contain max-width="300"></v-img>
       </v-col>
       <v-col v-else cols="12" xl="2" lg="2" md="2" sm="12" xs="12">
-        <v-img src="img/erasmusLogoFooter.jpg" contain height="100px" width="250px"></v-img>
-      </v-col>
-      <v-spacer></v-spacer>
-      <!-- EU Logo -->
-      <v-col cols="12" xl="2" lg="2" md="2" sm="12" xs="12" v-if="$vuetify.breakpoint.mdAndDown">
-        <!-- <v-img src="img/EU_POS.png" contain max-width="150"></v-img> -->
-      </v-col>
-      <v-col v-else cols="12" xl="2" lg="2" md="2" sm="12" xs="12">
-        <!-- <v-img src="img/EU_POS.png" contain height="90" class="mt-5"></v-img> -->
-      </v-col>
-      <!-- Text -->
-      <!-- <v-col cols="12" xl="8" lg="8" md="8" sm="12" xs="12" class="pl-0 pt-3" v-if="$vuetify.breakpoint.mdAndDown">
-        <p class="footerText mb-0 pb-0" style="font-weight: bold;">Contact info: </p>
-        <p class="footerText ma-0 pa-0 ml-5">   Progress Consult    </p>
-        <p class="footerText ma-0 pa-0 ml-5">   1046 Budapest, Klauzál u. 10.    </p>
-        <p class="footerText ma-0 pa-0 ml-5">   + 36 1 399 0772    </p>
-        <p class="footerText ma-0 pa-0 ml-5">   
-          <a href="progress@progress.no" target="_blank">progress@progress.no</a>  
-        </p>
-      </v-col> -->
-      <!-- <v-col cols="12" xl="8" lg="8" md="8" sm="12" xs="12" class="pl-15 pt-3" v-else>
-        <p class="footerText mb-0 pb-0" style="font-weight: bold;">Contact info: </p>
-        <p class="footerText ma-0 pa-0 ml-5">   Progress Consult    </p>
-        <p class="footerText ma-0 pa-0 ml-5">   1046 Budapest, Klauzál u. 10.    </p>
-        <p class="footerText ma-0 pa-0 ml-5">   + 36 1 399 0772    </p>
-        <p class="footerText ma-0 pa-0 ml-5">   
-          <a href="progress@progress.no" target="_blank">progress@progress.no</a>  
-        </p>
-      </v-col> -->
-      <!-- Image -->
-      <!-- <v-col cols="12" xl="2" lg="2" md="2" sm="12" xs="12">
         <v-img
-          v-if="$vuetify.breakpoint.mdAndDown"
-          class="mt-5 mb-5"
-          src="img/newLogo.jpg"
-          max-height="150"
-          max-width="150"
+          src="img/erasmusLogoFooter.jpg"
           contain
+          height="100px"
+          width="250px"
         ></v-img>
-        <v-img
-          v-else
-          class="mt-5"
-          src="img/newLogo.jpg"
-          max-height="130"
-          max-width="130"
-          contain
-        ></v-img>
-      </v-col> -->
+      </v-col>
     </v-row>
   </v-footer>
 </template>
